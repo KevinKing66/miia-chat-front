8222db44c0c6f989d4b5e000b067bde8b7945cf7
KevinKing66 <kevinvallenato2002@gmail.com>
2025-03-12 16:22:10 -0500

    se borra header antiguo

--- src/components/miia-header.ts
+++ src/components/miia-header.ts
@@ -7,7 +7,16 @@ export class MiiaHeader extends LitElement {
     protected render() {
 
         return html`
-        <nav class="row navbar navbar-scrolled">
+        <!-- <div class="header" style="display: flex; color: white; align-items: center;justify-content: space-between;background-color:var(--fourh-color);padding: 5px;font:1.2rem Fira Sans,sans-serif;">
+            <div style="display: flex;flex-direction: row;">
+                <h1><strong>MIIA</strong></h1>
+                <img src="assets/logo_horizontal.png" alt="" srcset="" style="height: 2rem;">
+            </div>
+            <div style="margin-left: 10px;font-size: 1.5rem;">                    
+                <a href="/logout" id="qsLogoutBtn" style="color: white;"><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+            </div>
+        </div> -->
+        <nav class="row navbar navbar-scrolled" style="--bs-gutter-x:0;">
             <div class="col-3">
                 <a class="navbar-brand" href="#">
                     <img src="media/images/logo-white.svg"  alt="Comtor" style="margin: 10px 5px 5px 10px; height: 40px; ">
@@ -17,7 +26,8 @@ export class MiiaHeader extends LitElement {
             <div class="profile-options col-6 justify-content-end">
                 <user-status-widget></user-status-widget> 
             </div>    
-        </nav>`;
+        </nav>
+        `;
     }
 
 
