6b94502d1e597af01ab977800f86299185744193
KevinKing66 <kevinvallenato2002@gmail.com>
2025-03-15 21:58:24 -0500

    re factor y se usan componentes creados paara simplificar codigo

--- src/components/chat.ts
+++ src/components/chat.ts
@@ -1,86 +1,41 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css, PropertyValues } from 'lit';
 import { customElement, property, query, state } from 'lit/decorators.js';
-import { v4 as uuidv4 } from "uuid";
 import { AuthResponse, Message, MessageDto, ReplyDTO } from '../dto/chat';
 
 @customElement("chat-component")
 export class ChatComponent extends LitElement {
-  @state()
-  ngrok_url: string = "https://equal-katydid-harmless.ngrok-free.app";
-
-  miia_url: string = "https://miia.comtor.net/miiaapi/chatbot-web/";
-  // miia_url: string = "http://127.0.0.1:9192/chatbot-web/";
+  readonly ngrok_url = "https://equal-katydid-harmless.ngrok-free.app";
+  readonly miia_url = "https://miia.comtor.net/miiaapi/chatbot-web/";
 
   @state() private messages: Array<Message> = [];
-  @query("#chatInput") textarea!: HTMLInputElement;
 
 
-  private fetchData(): void{
+  fetchData(): void {
     fetch(`${this.ngrok_url}/user-info`)
-  .then(response => response.json())
-  // .then(data => {console.log("Data: ", data)})
-  .then(data => {
-    sessionStorage.credentials = JSON.stringify(data);
-    let msg: Message = { text: `Hola ${data.userinfo.nickname}, ¿Cómo puedo ayudarte hoy?`, type: 'BOT' }
-    this.messages = [msg];
-
-  })
-  .catch(error => console.error("Error en la solicitud:", error));
+      .then(response => response.json())
+      .then(data => {
+        sessionStorage.credentials = JSON.stringify(data);
+        let msg: Message = { text: `Hola ${data.userinfo.nickname}, ¿Cómo puedo ayudarte hoy?`, type: 'BOT' };
+        let msg2: Message = { text: "Texto con **negrita**, *cursiva* y `código a copiar`.", type: 'BOT' };
+        this.messages = [msg, msg2];
+      })
+      .catch(error => console.error("Error en la solicitud:", error));
   }
 
-  private getCredentials(): AuthResponse{
-    let credentials: AuthResponse = JSON.parse(sessionStorage.credentials);
-    return credentials;
+  private updateMessages(event: CustomEvent) {
+    console.log("Evento recibido en el padre:", event.detail);
+    this.messages = [...event.detail]; // Actualiza los mensajes correctamente
   }
 
-  private sendMessage(): void {
-    if (this.textarea.value.trim()) {
-      let credential = this.getCredentials();
-      this.messages = [...this.messages, { text: this.textarea.value.trim(), type: 'USER' }];
-      let now = Math.floor(Date.now() / 1000)
-      const body: MessageDto = { to_user_code: credential.userinfo.email, to_user_name: credential.userinfo.nickname, message: this.textarea.value, chatbot_code: "COMTOR", sent_timestamp: now.toString() };
-      this.textarea.value = '';
-      const reqInit = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      }
-      fetch(this.miia_url, reqInit)
-        .then(resp => resp.json())
-        .then(data => {
-          console.log("Respuesta:");
-          console.log(JSON.stringify(data))
-          const reply: ReplyDTO = JSON.parse(data)[0] as ReplyDTO;
-          console.log("Reply: ", reply)
-          this.messages = [...this.messages, { text: reply.answer, type: 'BOT' }];
-        });
-    }
-  }
 
-  handleKeyDown(event: KeyboardEvent) {
-    if (event.key === "Enter") {
-      console.log("Enter pressed:", this.textarea.value);
-      this.sendMessage();
-    }
-  }
-
-  parseBold(text: string): string {
-    return text.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
-  }
 
   render() {
     return html`
       <div class="chat-content">
-        <div class="chat-container">
-          ${this.messages.map(msg => html`<div class="message ${msg.type.toLocaleLowerCase()}"><p .innerHTML=${this.parseBold(msg.text)}></p></div>`)}
-        </div>
-        
-        <div class="input-container">
-          <textarea id="chatInput" placeholder="Escribe un mensaje..." rows="1" style="resize: true;" @keydown=${this.handleKeyDown}></textarea>
-          <button @click="${this.sendMessage}">Enviar</button>
-        </div>
+
+        <messages-container .messages="${this.messages}" class="chat-container"></messages-container>
+
+        <input-message-component .messages="${this.messages}" @messages-updated="${this.updateMessages}"></input-message-component>
       </div>
     `;
   }
