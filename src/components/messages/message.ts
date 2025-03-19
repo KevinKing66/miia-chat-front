180fa2a952dd1a40ec6e93fced887ffc70622685
KevinKing66 <kevinvallenato2002@gmail.com>
2025-03-19 15:18:02 -0500

    se agg animacion

--- src/components/messages/message.ts
+++ src/components/messages/message.ts
@@ -7,41 +7,55 @@ export class MessageComponent extends LitElement {
     @property({ type: Object })
     msg?: Message;
 
-    private parseText(text?: string): string {
-        if (text == null) {
-            return '';
-        }
+    @state()
+    private displayedText: string = "";
 
+    private parseText(text: string): string {
         return text
             .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>') // Negrita
             .replace(/\*(.*?)\*/g, '<em>$1</em>') // Cursiva
             .replace(/`([\s\S]*?)`/g, (_, code) => {
-                const escapedCode = code.replace(/&/g, '&amp;') // Escapar caracteres especiales
+                const escapedCode = code.replace(/&/g, '&amp;')
                     .replace(/</g, '&lt;')
                     .replace(/>/g, '&gt;')
                     .replace(/"/g, '&quot;');
 
                 return `
         <div class="code-container">
-          <!--- <button class="copy-btn" @click=${() => this.copyToClipboard(code)}>📋</button> ---!>
           <pre><code>${escapedCode}</code></pre>
         </div>`;
             });
     }
 
-    private copyToClipboard(text: string = this.msg!.text) {
-        navigator.clipboard.writeText(text)
-            .then(() => console.log("Texto copiado:", text))
-            .catch(err => console.error("Error al copiar:", err));
+    connectedCallback() {
+        super.connectedCallback();
+        this._startTypingAnimation();
+    }
+
+    private _startTypingAnimation() {
+        if (!this.msg?.text) return;
+
+        let index = 0;
+        const interval = setInterval(() => {
+            if (index < this.msg!.text.length) {
+                this.displayedText = this.msg!.text.substring(0, index + 1);
+                index++;
+            } else {
+                clearInterval(interval); // Detiene la animación cuando el texto está completo
+            }
+        }, 50);
     }
 
     render() {
         return html`
             <div class="message-options-container">
-                <p .innerHTML=${this.parseText(this.msg?.text)}></p>
+                ${
+                    this.msg?.type === "BOT" ? html`<p .innerHTML=${this.parseText(this.displayedText)}></p>` 
+                    : html`<p .innerHTML=${this.parseText(this.msg?.text)}></p>`
+                }
                 <options-messages-component .msg=${this.msg}></options-messages-component>
             </div>
-            `;
+        `;
     }
 
     protected createRenderRoot() {
