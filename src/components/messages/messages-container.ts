22eafce71ccf66bb0b383a7f596510f70e87a0a4
KevinKing66 <kevinvallenato2002@gmail.com>
2025-03-19 11:43:49 -0500

    Manejo de estado entre container, input  y chat-container-msg

--- src/components/messages/messages-container.ts
+++ src/components/messages/messages-container.ts
@@ -1,43 +1,49 @@
 import { html, LitElement, PropertyValues } from 'lit';
 import { property, state, customElement, query } from 'lit/decorators.js';
-import { Message } from '../dto/chat';
+import { Message } from '../../dto/chat';
 
 @customElement("messages-container")
-export class MessageContainer extends LitElement{
+export class MessageContainer extends LitElement {
 
-    @property() 
-    private messages: Array<Message> = [];
+  @property()
+  private messages: Array<Message> = [];
 
-    @query("#chatContainerMsg") chatContainer!: HTMLElement;
-      
-      private scrollToBottom() {
-        if (this.chatContainer) {
-            requestAnimationFrame(() => {
-                this.chatContainer.scrollTop = this.chatContainer.scrollHeight;
-            });
-        }
-      }
-    
-      protected firstUpdated(_changedProperties: PropertyValues): void {
-        this.scrollToBottom();
-      }
-      
-      updated(changedProperties: Map<PropertyKey, unknown>) {
-        if (changedProperties.has("messages")) {
-          this.scrollToBottom();
-        }
-      }
+  @property()
+  status: "LOADING" | "FREE" = "FREE";
 
-    protected render() {
-        return html`
-          <div class="chat-container-msg" id="chatContainerMsg">
-            ${this.messages.map(
-                msg => html`<message-component class="message ${msg.type.toLocaleLowerCase()}" .msg="${msg}"></message-component>`
-            )}
-          </div>`;
+  @query("#chatContainerMsg") chatContainer!: HTMLElement;
+
+  private scrollToBottom() {
+    if (this.chatContainer) {
+      requestAnimationFrame(() => {
+        this.chatContainer.scrollTop = this.chatContainer.scrollHeight;
+      });
     }
+  }
 
-    protected createRenderRoot() {
-        return this;
+  protected firstUpdated(_changedProperties: PropertyValues): void {
+    this.scrollToBottom();
+  }
+
+  updated(changedProperties: Map<PropertyKey, unknown>) {
+    if (changedProperties.has("messages")) {
+      this.scrollToBottom();
     }
+  }
+
+  protected render() {
+    return html`
+      <div class="chat-container-msg" id="chatContainerMsg">
+        ${this.messages.map(
+          msg => html`<message-component class="message ${msg.type.toLocaleLowerCase()}" .msg="${msg}"></message-component>`
+        )}
+        ${
+          this.status === "LOADING" ? html`<loading-message></loading-message>` : html``
+        }
+      </div>`;
+  }
+
+  protected createRenderRoot() {
+    return this;
+  }
 }
