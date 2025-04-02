1c219f9a711b28166229daa1bb544527073a58c9
KevinKing66 <kevinvallenato2002@gmail.com>
2025-04-02 17:50:39 -0500

    se adapta a cambios hechos en express

--- login_auth0/wsp_qr_api.py
+++ login_auth0/wsp_qr_api.py
@@ -1,41 +1,32 @@
 from fastapi import FastAPI, Response
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, JSONResponse
 import httpx
 import qrcode
 from io import BytesIO
 
 app = FastAPI()
 
-@app.get("/qr/generate/")
-async def call_qr():
-    async with httpx.AsyncClient() as client:
-        response = await client.get("http://localhost:8080/qr")
-        return response.json()
-
-@app.get("/qr/generate/image")
-async def show_qr(resp: Response):
+@app.get("/bot/qr/image/{uuid}")
+async def show_qr(uuid:str):
     qr = None
     status = None
     msg = None
     async with httpx.AsyncClient() as client:
-        response = await client.get("http://localhost:8080/qr")
+        response = await client.get(f"http://localhost:8080/whatsapp_session/{uuid}")
         data = response.json()
         qr = data.get("qr")
         status = data.get("status")
-        status = data.get("status")
-        resp.status_code = response.status_code
         if(response.status_code != 200):
             msg = data.get("message")
     
-    if status == "BOT_CONNECTED" or status == "NON-EXISTENT_QR":
-        return {"message": status}
+    if status == "BOT_CONNECTED":
+        return JSONResponse(content={"message": "El bot ya está conectado"}, status_code=403)
     
-    if  qr == None:
-        return {"message": "No se ha podido generar el QR"}
-    
-    if  status == None:
-        # resp.status_code = 500
-        return {"message": "Error con el servidor"}
+    if status == "NON-EXISTENT_QR":
+        return JSONResponse(content={"message": "El QR aún no está disponible"}, status_code=404)
+
+    if qr is None:
+        return JSONResponse(content={"message": msg}, status_code=500)
 
     img = qrcode.make(qr)
 
@@ -44,9 +35,16 @@ async def show_qr(resp: Response):
     img.save(buf)
     buf.seek(0)
 
-    # Retornar la imagen en la respuesta HTTP
-    return StreamingResponse(buf, media_type="image/png")
+    return StreamingResponse(buf, media_type="image/png", status_code=200)
     
+
+@app.get("/bot/qr/{uuid}")
+async def call_qr(uuid: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"http://localhost:8080/whatsapp_session/{uuid}")
+        return response.json()
+
+
 @app.get("/hello")
 async def hello():
     # Generar el QR con el texto "hola"
